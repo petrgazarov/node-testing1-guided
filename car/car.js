1b84ea17ccb30f2e95ef1c648b9b62371043df8e
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-05-19 19:26:13 +0200

    wip

--- car/car.js
+++ car/car.js
@@ -1,14 +1,14 @@
 class Car {
 
   constructor(make, model) {
-    this.model = model
-    this.make = make
-    this.odometer = 0
     this.owner = "Dan"
+    this.model = model
+    this.odometer = 0
+    this.make = make
   }
 
-  drive() {
-
+  drive(miles) {
+    this.odometer += miles
   }
 }
 
