b4f1269bfba2dc470b907e9c19652c1ca1baf0ee
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-05-19 18:30:05 +0200

    wip

--- car/car.spec.js
+++ car/car.spec.js
@@ -1,15 +1,17 @@
 function foo() {
   return 5
 }
+function bar() {
+  return {}
+}
 
 // Test away!
 describe('our first tests', () => {
 
-  it('works', () => {
-
+  it('foo works', () => {
     expect(foo).toBeDefined() // assertion
     expect(foo).toBeTruthy() // another assertion
-    expect(foo).toBe(5)      // should be related to the ONE thing
+    expect(foo()).toBe(5)      // should be related to the ONE thing
   })
 
   test('works some more', () => {
