b365848698401d3cbc8c902294087c04b15e1c97
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-05-19 18:42:28 +0200

    wip

--- car/car.spec.js
+++ car/car.spec.js
@@ -7,19 +7,16 @@ function bar() {
 
 // Test away!
 describe('our first tests', () => {
-
   it('foo returns five', () => {
     expect(foo).toBeDefined() // assertion
-    expect(foo).toBeTruthy() // another assertion
-    expect(foo()).toBe(5)      // should be related to the ONE thing
+    expect(foo).toBeTruthy()  // another assertion
+    expect(foo()).toBe(5)     // should be related to the ONE thing
   })
-
   test('bar works', () => {
     expect(bar).toBeDefined()
     expect(bar).toBeTruthy()
     expect(bar()).not.toBe({}) // only if truly same object passes
-    expect(bar()).toEqual({}) // keys and values the same
+    expect(bar()).toEqual({})  // keys and values the same
     expect(bar()).toMatchObject({})
   })
-
 })
