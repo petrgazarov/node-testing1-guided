f1a131700f15a874c38f9207011baa9db583eeec
Petr Gazarov <petrgazarov@gmail.com>
2021-10-04 19:30:48 -0700

    Implement divide function

--- helpers/math-helpers.js
+++ helpers/math-helpers.js
@@ -6,9 +6,13 @@ function sum(x, y) {
 // subtract (x, y)
 
 // divide (x, y)
+function divide(x, y) {
+  return x / y;
+}
 
 // multiply (x, y)
 
 module.exports = {
   sum,
+  divide,
 };
--- helpers/math-helpers.spec.js
+++ helpers/math-helpers.spec.js
@@ -1,8 +1,8 @@
 const { sum } = require('./math-helpers');
 
-describe('sum()', () => {
-  it('adds up two positive numbers', () => {
-    expect(sum(2, 3)).toBe(5);
+describe('sum()', () => { // test group
+  it('adds up two positive numbers', () => { // test (test case, test example)
+    expect(sum(2, 3)).toBe(5); // assertion
   });
   
   it('adds up two numbers: one negative and one positive', () => {
@@ -11,5 +11,23 @@ describe('sum()', () => {
   
   it('adds up two negative numbers', () => {
     expect(sum(-2, -3)).toBe(-5);
+  });
+});
+
+describe('divide()', () => {
+  it.skip('divides positive numbers', () => {
+
+  });
+
+  it.skip('divides a positive number by a negative number', () => {
+
+  });
+
+  it.skip('divides a negative number by a positive number', () => {
+
+  });
+
+  it.skip('divides negative numbers', () => {
+
   });
 });
